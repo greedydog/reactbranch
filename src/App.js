80780aa74d6e053b6d52df019566e00a2534242b
lee <greedydog41@gmail.com>
2023-08-07 16:46:00 +0900

    'asdg'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+
+import react, {useEffect, useState} from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+    const [data, setData] = useState({});
+     const url = "http://127.0.0.1:5021/news_list";
+
+      axios.defaults.withCredentials = true;
+  
+
+useEffect(async () => {
+  const posts = await axios.get("https://jsonplaceholder.typicode.com/users/1/posts");
+  setPosts(posts.data);
+}, []);
+
+
+
+
+     const getAxiosData = async () => {  
+      
+      
+      axios.get(url)
+      .then(function(data){
+         
+         displayJSONData(data)
+          console.log(data)
+      })
+      
+      .catch(function(e) {
+          console.log(e);
+      })
+      // setData(await data.json())
+    }
+    
+    function displayJSONData(data) {
+      
+      for (let i=0;i<data.length; i++)
+          {
+              if (data[i]['prediction'] === 1){
+                  const li = document.createElement("li");
+                  li.setAttribute('id', i);
+                  const textNode = document.createTextNode(JSON.stringify(data[i]['article']));
+                  li.appendChild(textNode);
+                  document.getElementById('fruits').appendChild(li)
+              }
+          }
+      console.log(data)
+
+  }
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="fruits"></div>
     </div>
-  );
+  )
+    
 }
 
 export default App;
