18465e69b2a701c5550b02709a4be0f46ff683fe
lee <greedydog41@gmail.com>
2023-08-09 17:52:34 +0900

    asdf

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
 
 import react, {useEffect, useState} from 'react'
+
 import axios from 'axios'
 import './App.css';
 
 function App() {
-    const [data, setData] = useState({});
-     const url = "http://127.0.0.1:5021/news_list";
+//     const [data, setData] = useState({});
 
-      axios.defaults.withCredentials = true;
+//       axios.defaults.withCredentials = true;
   
+// const getData = async () => {
+//   const data = await axios.get(
+//     "http://127.0.0.1:5021/news_list"
+//   );
+//   setData(data);
+// };
 
-useEffect(async () => {
-  const posts = await axios.get("https://jsonplaceholder.typicode.com/users/1/posts");
-  setPosts(posts.data);
-}, []);
+// useEffect(() => {
+//   getData(data);
+//   displayJSONData(data)
+// }, [data]);
 
-
-
-
+// console.log(data)
+const url = "http://127.0.0.1:5021/news_list"
      const getAxiosData = async () => {  
       
       
       axios.get(url)
       .then(function(data){
          
-         displayJSONData(data)
+        //  displayJSONData(data)
           console.log(data)
       })
       
@@ -34,21 +39,27 @@ useEffect(async () => {
       // setData(await data.json())
     }
     
-    function displayJSONData(data) {
-      
-      for (let i=0;i<data.length; i++)
-          {
-              if (data[i]['prediction'] === 1){
-                  const li = document.createElement("li");
-                  li.setAttribute('id', i);
-                  const textNode = document.createTextNode(JSON.stringify(data[i]['article']));
-                  li.appendChild(textNode);
-                  document.getElementById('fruits').appendChild(li)
-              }
-          }
-      console.log(data)
 
-  }
+getAxiosData()
+
+
+
+
+  //   function displayJSONData(data) {
+      
+  //     for (let i=0;i<data.length; i++)
+  //         {
+  //             if (data[i]['prediction'] === 1){
+  //                 const li = document.createElement("li");
+  //                 li.setAttribute('id', i);
+  //                 const textNode = document.createTextNode(JSON.stringify(data[i]['article']));
+  //                 li.appendChild(textNode);
+  //                 document.getElementById('fruits').appendChild(li)
+  //             }
+  //         }
+  //     console.log(data)
+
+  // }
 
 
   
